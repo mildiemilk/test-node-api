9cc9152c4c3196934ce10c173ae12ab1c4ea6cee
mildiemilk <punthitra.dits@gmail.com>
2020-05-15 00:13:21 +0700

    Add mongoose

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// localhost:3000/users
+router.get('/', function (req, res, next) {
+  res.status(200).json({
+    message: 'Success'
+  })
+});
+
+// localhost:3000/users/login
+router.get('/login', function (req, res, next) {
+  res.status(200).json({
+    message: 'Login'
+  })
 });
 
 module.exports = router;
