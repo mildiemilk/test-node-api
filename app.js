2614cbfab17dcdfe54aa152a3ab22bdc1a2a282d
mildiemilk <punthitra.dits@gmail.com>
2020-05-25 23:00:02 +0700

    Upload image

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ mongoose.connect('mongodb+srv://admin:admin@clustertest-wy2pg.mongodb.net/test_n
 });
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({
+  limit: '50mb'
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- controllers/shopControllers.js
+++ controllers/shopControllers.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const uuidv4 = require('uuid');
+const { promisify } = require('util')
+const writeFileAsync = promisify(fs.writeFile)
+const { Storage } = require('@google-cloud/storage');
+const stream = require('stream');
 const Shop = require('../models/shop')
 const Menu = require('../models/menu')
 
@@ -8,7 +15,7 @@ exports.index = async (req, res, next) => {
     return {
       id: item._id,
       name: shop.name,
-      photo: `http://localhost:3000/images/ ${item.photo}`,
+      photo: `http://localhost:3000/images/${item.photo}`,
       location: shop.location,
     }
   }
@@ -45,13 +52,105 @@ exports.getShopWithMenu = async (req, res, next) => {
 }
 
 exports.insert = async (req, res, next) => {
-  const { name, location } = req.body
+  const { name, location, photo } = req.body
   const shop = new Shop({
     name,
-    location
+    location,
+    photo: await saveImageToDisk(photo),
   })
   await shop.save();
   res.status(201).json({
     message: 'Add shop done'
   })
+}
+
+// ep 14
+async function saveImageToGoogle(baseImage) {
+  //หา path จริงของโปรเจค
+  const projectPath = path.resolve('./');
+
+  //หานามสกุลไฟล์
+  const ext = baseImage.substring(baseImage.indexOf("/") + 1, baseImage.indexOf(";base64"));
+  console.log(ext);
+
+  //สุ่มชื่อไฟล์ใหม่ พร้อมนามสกุล
+  let filename = '';
+  if (ext === 'svg+xml') {
+    filename = `${uuidv4.v4()}.svg`;
+  } else {
+    filename = `${uuidv4.v4()}.${ext}`;
+  }
+
+  //Extract base64 data ออกมา
+  let image = decodeBase64Image(baseImage);
+
+  const bufferStream = new stream.PassThrough();
+  bufferStream.end(Buffer.from(image.data, 'base64'));
+
+  // Creates a client and upload to storage
+  const storage = new Storage({
+    projectId: 'enhanced-idiom-112215',
+    keyFilename: `${projectPath}/key.json`
+  });
+
+  const myBucket = storage.bucket('node_api_file');
+  var newFilename = myBucket.file(filename);
+  bufferStream.pipe(newFilename.createWriteStream({
+    gzip: true,
+    contentType: image.type,
+    metadata: {
+      // Enable long-lived HTTP caching headers
+      // Use only if the contents of the file will never change
+      // (If the contents will change, use cacheControl: 'no-cache')
+      cacheControl: 'public, max-age=31536000',
+    },
+    public: true,
+    validation: "md5"
+  }).on('error', (err) => {
+    console.log('err =>' + err);
+  }).on('finish', () => {
+    console.log('upload successfully...');
+  }));
+
+  //return ชื่อไฟล์ใหม่ออกไป
+  return filename;
+}
+
+async function saveImageToDisk(baseImage) {
+  //หา path จริงของโปรเจค
+  const projectPath = path.resolve('./');
+  //โฟลเดอร์และ path ของการอัปโหลด
+  const uploadPath = `${projectPath}/public/images/`;
+
+  //หานามสกุลไฟล์
+  const ext = baseImage.substring(baseImage.indexOf("/") + 1, baseImage.indexOf(";base64"));
+
+  //สุ่มชื่อไฟล์ใหม่ พร้อมนามสกุล
+  let filename = '';
+  if (ext === 'svg+xml') {
+    filename = `${uuidv4.v4()}.svg`;
+  } else {
+    filename = `${uuidv4.v4()}.${ext}`;
+  }
+
+  //Extract base64 data ออกมา
+  let image = decodeBase64Image(baseImage);
+
+  //เขียนไฟล์ไปไว้ที่ path
+  await writeFileAsync(uploadPath + filename, image.data, 'base64');
+  //return ชื่อไฟล์ใหม่ออกไป
+  return filename;
+}
+
+function decodeBase64Image(base64Str) {
+  var matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  var image = {};
+  if (!matches || matches.length !== 3) {
+    throw new Error('Invalid base64 string');
+  }
+
+  image.type = matches[1];
+  image.data = matches[2];
+
+  return image;
 }
