9ff1b4fbee4b6478de60da31072c12b29ad1d8f6
mildiemilk <punthitra.dits@gmail.com>
2020-06-04 00:06:53 +0700

    Create register user route

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const userControllers = require('../controllers/userControllers')
 /* GET users listing. */
 
-// localhost:3000/users
+// localhost:3000/user
 router.get('/', userControllers.index);
 
-// localhost:3000/users/login
+// localhost:3000/user/login
 router.get('/login', userControllers.login);
+// localhost:3000/user/register
+router.post('/register', userControllers.register);
 
 module.exports = router;
