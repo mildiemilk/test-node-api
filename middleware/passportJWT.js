697f7af2b3effc2f0ca9051a02800c888131f99f
mildiemilk <punthitra.dits@gmail.com>
2020-06-13 19:55:35 +0700

    Add function to validate Token

---
+++ middleware/passportJWT.js
@@ -0,0 +1,24 @@
+const passport = require('passport')
+const config = require('../config/index')
+const User = require('../models/user')
+
+var JwtStrategy = require('passport-jwt').Strategy,
+  ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.JWT_SECRET;
+// opts.issuer = 'accounts.examplesoft.com';
+// opts.audience = 'yoursite.net';
+passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+  try {
+    const user = await User.findById(jwt_payload.id)
+    if (!user) {
+      return done(new Error('user not forn'), null)
+    }
+    return done(null, user)
+  } catch (error) {
+    done(error)
+  }
+}));
+
+module.exports.isLogin = passport.authenticate('jwt', { session: false })
