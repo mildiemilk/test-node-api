8acb03161f87b8ee2c33bded44a51798001d8358
mildiemilk <punthitra.dits@gmail.com>
2020-06-13 19:56:19 +0700

    Apply passport jwt middleware with getStaff

--- routes/staff.js
+++ routes/staff.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const staffControllers = require('../controllers/staffControllers')
+const passportJWT = require('../middleware/passportJWT')
 /* GET users listing. */
 
-// localhost:3000/users
-router.get('/', staffControllers.index);
+// localhost:3000/staff
+router.get('/', passportJWT.isLogin, staffControllers.index);
 router.get('/:id', staffControllers.show);
 router.post('/', staffControllers.insert);
 router.delete('/:id', staffControllers.destroy);
 router.put('/:id', staffControllers.update);
 
-// localhost:3000/users/login
+// localhost:3000/staff/login
 
 module.exports = router;
